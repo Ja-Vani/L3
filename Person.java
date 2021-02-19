b33b891d390df9961a4c05c018ae34ffe9aefa3e
Ja-Vani <73061981+Ja-Vani@users.noreply.github.com>
2021-02-19 17:59:36 +0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,16 @@
+
+public interface Person {
+	String move();
+
+	void like(SongSing song, String how);
+
+	void walk(Places place);
+
+	void think();
+
+	void sing(SongSing song, String how);
+
+	void sit(Places place);
+
+	void ready();
+}
